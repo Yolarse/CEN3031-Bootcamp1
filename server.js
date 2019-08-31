cb8c56bd43852d65a571035e87062c5218fe74b9
Yolarse <mikejlee17@gmail.com>
2019-08-30 20:33:09 -0400

    Completed Bootcamp 1

--- server.js
+++ server.js
@@ -5,10 +5,8 @@ var http = require('http'),
 
 /* Global variables */
 var listingData, server;
-
 var requestHandler = function(request, response) {
-  var parsedUrl = url.parse(request.url);
-
+	var parsedUrl = url.parse(request.url);
   /*
     Your request handler should send listingData in the JSON format as a response if a GET request 
     is sent to the '/listings' path. Otherwise, it should send a 404 error. 
@@ -24,28 +22,37 @@ var requestHandler = function(request, response) {
     HINT: Explore the list of MIME Types
     https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Complete_list_of_MIME_types
    */
+   //console.log(parsedUrl);
+   if(parsedUrl.pathname == '/listings') {
+		response.writeHead(200, {'Content-Type': 'application/json'});
+		response.write(JSON.stringify(listingData));
+		response.end();
+   }
+   else {
+		response.writeHead(404); 
+		response.end('Bad gateway error');
+   }
+   
 };
 
 fs.readFile('listings.json', 'utf8', function(err, data) {
   /*
     This callback function should save the data in the listingData variable, 
     then start the server. 
-
     HINT: Check out this resource on fs.readFile
     //https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback
 
     HINT: Read up on JSON parsing Node.js
    */
-
     //Check for errors
-  
-
+	if (err) throw err;
    //Save the sate in the listingData variable already defined
-  
-
+  	listingData = JSON.parse(data);
   //Creates the server
-  
+ // console.log(listingData.entries);
+	var server = http.createServer(requestHandler);
   //Start the server
-
-
+	server.listen(port, function() {
+		console.log('Server listening on: http://127.0.0.1:' + port);
+	});
 });
